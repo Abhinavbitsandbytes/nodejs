967b735dc52e9719ce20db621c878b033b5dcff9
Abhinav <abhinav.gupta@precily.com>
2021-02-19 14:05:55 +0530

    11. Importing npm modules

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,10 +1,6 @@
+const validator = require('validator')
 const getNotes = require('./notes.js')
 
-
 console.log(getNotes())
-// Challange: Define and use a function in a new file
 
-// 1. Create a new file called notes.js
-// 2. create getNotes function that returns "Your notes..."
-// 3. export getNotes function
-// 4. from app.js, load in and call the function printing message to console
+console.log(validator.isEmail('agmail.com'))
