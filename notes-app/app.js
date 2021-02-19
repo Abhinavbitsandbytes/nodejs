739ddeaf4a2e6246d8405b05983013a7a9a57f25
Abhinav <abhinav.gupta@precily.com>
2021-02-19 14:22:55 +0530

    12. Printing in color

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,6 @@
-const validator = require('validator')
+
 const getNotes = require('./notes.js')
+const chalk = require('chalk')
 
-console.log(getNotes())
+console.log(chalk.green.inverse.bold('success yay!'))
 
-console.log(validator.isEmail('agmail.com'))
