a913f67a02cd78abe2574b96168dcda0229c3fab
Abhinav <abhinav.gupta@precily.com>
2021-02-19 13:41:17 +0530

    10. importing your own files

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,11 +1,10 @@
-const fs = require('fs')
-
-fs.writeFileSync('notes.txt', 'This file was created by Node.js')
-
-// challange - append a message to notes.txt
-
-// 1. use appendFileSync to append to the file
-
-fs.appendFileSync('notes.txt', 'from Abhinav')
+const getNotes = require('./notes.js')
 
 
+console.log(getNotes())
+// Challange: Define and use a function in a new file
+
+// 1. Create a new file called notes.js
+// 2. create getNotes function that returns "Your notes..."
+// 3. export getNotes function
+// 4. from app.js, load in and call the function printing message to console
