7e808e1e57825420a2e9791e6e181e2690714127
Abhinav <abhinav.gupta@precily.com>
2021-02-21 00:27:16 +0530

    48. Customizing the Views Directory

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const path = require('path')
 
-
 const app = express()
+
+// Define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates')
 
+// setup handlebars engine and views location
 app.set('view engine','hbs')
+app.set('views', viewsPath)
 
+// setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('/about', (req,res) =>{
