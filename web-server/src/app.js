fa6f806a7f15fcd6462795281a02059e0e0d4251
Abhinav <abhinav.gupta@precily.com>
2021-02-21 00:09:46 +0530

    47. Dynamic Pages with Templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,16 +5,30 @@ const path = require('path')
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine','hbs')
+
 app.use(express.static(publicDirectoryPath))
 
-//we can comment this as above line will look for index.html file and serve so commented code is rendundant
-
-// app.get('', (req, res) =>{
-//     res.send('<h1>hello express!</h1>')
-// })
-
-
-
+app.get('/about', (req,res) =>{
+   
+    res.render('about', {
+        title:"about",
+        name:"Abhinav"
+    })
+})
+app.get('/help', (req,res) =>{
+   
+    res.render('help', {
+        helpText:"This is some helpful text",
+    })
+})
+app.get('', (req,res) =>{
+   
+    res.render('index', {
+        title:"weather App",
+        name:"Abhinav"
+    })
+})
 app.get('/weather', (req,res)=>{
     res.send('weather')
 })
