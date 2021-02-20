b8ed5b768022c7dc6414c7d96f9e3811f5c79d2b
Abhinav <abhinav.gupta@precily.com>
2021-02-20 11:45:18 +0530

    31. Making HTTP Requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,11 @@
-console.log('starting')
 
-setTimeout(() => {
-    console.log('2 second timer')
-}, 2000);
 
-setTimeout(() => {
-    console.log('0 second timer')
-}, 0);
+const request = require('request')
 
-console.log('stopping')
+const url="http://api.weatherstack.com/current?access_key=babc4dc1a79130d598b11e87c6e850ab&query=37.8267,122.4233"
+
+request({url:url}, (error, response)=>{
+const data = JSON.parse(response.body)
+console.log(data.current)
+
+})
