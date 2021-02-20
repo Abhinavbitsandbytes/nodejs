4d6a4324bb9f7aa9b9fba971a9fc8b4c5618910d
Abhinav <abhinav.gupta@precily.com>
2021-02-20 12:31:42 +0530

    33. An HTTP Request Challenge

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,10 +2,19 @@
 
 const request = require('request')
 
-const url="http://api.weatherstack.com/current?access_key=babc4dc1a79130d598b11e87c6e850ab&query=37.8267,122.4233"
+const url="http://api.weatherstack.com/current?access_key=babc4dc1a79130d598b11e87c6e850ab&query=37.8267,122.4233&units=f"
 
-request({url:url}, (error, response)=>{
-const data = JSON.parse(response.body)
-console.log(data.current)
+request({url:url, json:true}, (error, response)=>{
+// console.log(response.body.current)
+
+})
+
+
+const geocodeURL="https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYWJoaW5hdmtyMTk3IiwiYSI6ImNrbGRjdnBhNzFxMGoycm4wbnc4aTFkeDUifQ.df-lr0eB9eZ1AuhRHNN-qw&limit=1"
+
+request({url:geocodeURL,json:true}, (error,response)=>{
+    const latitude = response.body.features[0].center[1]
+    const longitude = response.body.features[0].center[0]
+    console.log(latitude, longitude)
 
 })
