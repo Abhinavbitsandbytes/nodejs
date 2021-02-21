78536ee592a108029541f3fdd0d7d0fc9d20b6ae
Abhinav <abhinav.gupta@precily.com>
2021-02-21 13:55:34 +0530

    57. Browser HTTP Requests with Fetch

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,20 @@
 console.log('client side js is loaded')
+
+// fetch('http://puzzle.mead.io/puzzle').then((response) => {
+
+// response.json().then((data)=>{
+//     console.log(data)
+// })
+// })
+
+fetch('http://localhost:3000/weather?address=ballia').then((response)=>{
+
+    response.json().then((data)=>{
+        if(data.error){
+            console.log(data.error)
+        }
+        else{
+        console.log(data)
+        }
+    })
+})
