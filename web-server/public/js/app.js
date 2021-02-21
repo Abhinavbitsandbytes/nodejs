433ff976749c87d826c79a7176f0ba661d51af35
Abhinav <abhinav.gupta@precily.com>
2021-02-21 14:08:48 +0530

    58. Creating a Search Form

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -7,7 +7,15 @@ console.log('client side js is loaded')
 // })
 // })
 
-fetch('http://localhost:3000/weather?address=ballia').then((response)=>{
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+weatherForm.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    const location= search.value
+    console.log('testing', location)
+    fetch('http://localhost:3000/weather?address='+location).then((response)=>{
 
     response.json().then((data)=>{
         if(data.error){
@@ -18,3 +26,4 @@ fetch('http://localhost:3000/weather?address=ballia').then((response)=>{
         }
     })
 })
+} )
