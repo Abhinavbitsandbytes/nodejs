b9f9017c172b3ae539a31ab03a86100122a614c1
Abhinav <abhinav.gupta@precily.com>
2021-02-23 14:36:37 +0530

    81. Deleting Documents

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -15,32 +15,22 @@ MongoClient.connect(
     }
     const db = client.db(databaseName);
 
-//  db.collection('users').updateOne({
-//         _id: new ObjectID("6033ee3494eb2b311430781d")
-//     }, {
-//         $set:{
-//             name:'Mike'
-//         }
-//     }).then((result)=>{
-// console.log(result)
-//     }).catch((error)=>{
-// console.log(error)
-//     })
+// db.collection('users').deleteMany({
+//     age:27
+// }).then((result)=>{
+//     console.log(result)
+// }).catch((error)=>{
+//     console.log(error)
+// })
 
-// Goal: Use updateMany to complete all tasks
-// 1. check the document for updateMany
-
-db.collection('tasks').updateMany({
-    completed:false
-}, {
-    $set:{
-        completed:true
-    }
-}).then((result) =>{
+db.collection('tasks').deleteOne({
+    description:'this is description 2'
+}).then((result)=>{
     console.log(result)
 }).catch((error)=>{
     console.log(error)
 })
+
 
 
 
