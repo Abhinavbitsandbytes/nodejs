22ab22221afb906555fd0fd78cbd3b6855b96d1d
Abhinav <abhinav.gupta@precily.com>
2021-02-24 02:27:12 +0530

    91. Resource Reading Endpoints: Part I

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -27,6 +27,26 @@ task.save().then(()=>{
 })
 })
 
+app.get('/users', (req,res)=>{
+    User.find({}).then((users)=>{
+       
+res.send(users)
+    }).catch((error)=>{
+        res.send(500).send()
+    })
+})
+
+app.get('/users/:id', (req,res)=>{
+    const _id = req.params.id
+    User.findById(_id).then((user)=>{
+        if(!user){
+          return  res.status(404).send()
+        }
+res.send(user)
+    }).catch((error)=>{
+        res.status(500).send()
+    })
+})
 app.listen(port, ()=>{
     console.log('server is up on port', port)
 })
