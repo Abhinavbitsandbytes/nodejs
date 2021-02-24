b351a10828e8806a3a2e509c6a0da2cdb620e87b
Abhinav <abhinav.gupta@precily.com>
2021-02-24 22:43:32 +0530

    96. Async/Await: Part II

--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -5,11 +5,24 @@ const User = require('../src/models/user')
 
 //we are not using $set operator for update of mongodb native driver because mongoose take care that for us
 
-User.findByIdAndUpdate('60351f4c638b937dfc9609e4', {age:1}).then((user)=>{
-    console.log(user)
-    return User.countDocuments({age:1})
-}).then((result)=>{
-    console.log(result)
+// User.findByIdAndUpdate('60351f4c638b937dfc9609e4', {age:1}).then((user)=>{
+//     console.log(user)
+//     return User.countDocuments({age:1})
+// }).then((result)=>{
+//     console.log(result)
+// }).catch((error)=>{
+//     console.log(error)
+// })
+
+const updateAgeAndCount = async (id,age) =>{
+
+    const user = await User.findByIdAndUpdate(id, {age})
+    const count = await User.countDocuments({age})
+    return count
+}
+
+updateAgeAndCount('60351f4c638b937dfc9609e4', 0).then((count)=>{
+    console.log("c",count)
 }).catch((error)=>{
-    console.log(error)
+    console.log("e",error)
 })
