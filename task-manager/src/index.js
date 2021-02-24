e25940927b2fe06f7bfe42df1fad4dfa25a46696
Abhinav <abhinav.gupta@precily.com>
2021-02-24 23:56:11 +0530

    100. Resource Deleting Endpoints

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 require('./db/mongoose')
 const User = require('./models/user')
 const Task = require('./models/task')
+const { findByIdAndDelete } = require('./models/user')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -74,6 +75,23 @@ res.status(400).send(e)
     }
 })
 
+app.delete('/users/:id', async (req, res)=>{
+
+    try{
+        const user = await User.findByIdAndDelete(req.params.id)
+        if(!user){
+            return res.status(404).send()
+        }
+
+        res.send(user)
+
+    }
+    catch(e){
+res.status(500).send()
+    }
+
+
+})
 
 app.post('/tasks', async (req,res)=>{
     const task = new Task(req.body)
@@ -140,6 +158,23 @@ app.patch('/tasks/:id', async (req, res)=>{
         }
     })
 
+    app.delete('/tasks/:id', async (req, res)=>{
+
+        try{
+            const task = await Task.findByIdAndDelete(req.params.id)
+            if(!task){
+                return res.status(404).send()
+            }
+    
+            res.send(task)
+    
+        }
+        catch(e){
+    res.status(500).send()
+        }
+    
+    
+    })
 
 
 
